e1ca9a917a305e3884e1a9d26f68f9decea2a4b4
Alex <the_hax@gmx.net>
2015-01-16 17:22:39 +0100

    Model-View-Controller
    Refit for MVC

--- web/game-entity.dart
+++ web/game-entity.dart
@@ -1,15 +1,23 @@
 part of game;
 
 class GameEntity {
-  Game game;
-  Vector2 size;
-  Vector2 center;
-  Vector2 velocity;
+	Vector2 size;
+	Vector2 center;
+	Vector2 velocity;
 
-  GameEntity(Game this.game, Vector2 this.center, Vector2 this.size, Vector2 this.velocity) {}
+	GameEntity(Vector2 this.center, Vector2 this.size, Vector2 this.velocity) {}
 
-  update() {
-    center.add(velocity);
-  }
+	update() {
+		center.add(velocity);
+	}
 
+	bool isCollidingWith(GameEntity e) {
+		return !(
+			this == e ||
+			this.center.x + this.size.x / 2 < e.center.x - e.size.x / 2 ||
+			this.center.y + this.size.y / 2 < e.center.y - e.size.y / 2 ||
+			this.center.x - this.size.x / 2 > e.center.x + e.size.x / 2 ||
+			this.center.y - this.size.y / 2 > e.center.y + e.size.y / 2
+		);
+	}
 }
